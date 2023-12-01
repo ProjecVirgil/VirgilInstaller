a05adcaea4db184374fe5b1eeb2183259f2e5cdc
Retr0 <dragonfaiver@gmail.com>
2023-12-01 22:54:50 +0100

    Refactor the entire project
    Now the project is made with - Electron, Vite,React And for the build electron-builder

---
+++ src/renderer/src/App.jsx
@@ -0,0 +1,11 @@
+import Components from './components/Components'
+
+function App() {
+  return (
+    <div className="container">
+      <Components></Components>
+    </div>
+  )
+}
+
+export default App
---
+++ src/renderer/src/components/Components.jsx
@@ -0,0 +1,4 @@
+function Test() {
+  return <h1>TEST</h1>
+}
+export default Test
