86576e103a558db9261f061be995fbf7c64321a6
Retr0 <dragonfaiver@gmail.com>
2023-12-02 12:40:44 +0100

    Modified the size of window

--- src/main/index.js
+++ src/main/index.js
@@ -6,8 +6,8 @@ import icon from '../../resources/icons/icon.png?asset'
 function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 670,
+    width: 900,
+    height: 570,
     resizable: false,
     roundedCorners: true,
     show: false,
