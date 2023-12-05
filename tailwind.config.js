4f09bf964fb579cdabfab1bdd73a496eaba14910
Retr0 <dragonfaiver@gmail.com>
2023-12-05 22:07:30 +0100

    add theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-const { nextui } = require("@nextui-org/react");
+const { nextui } = require('@nextui-org/react')
 module.exports = {
   content: [
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
@@ -10,5 +10,41 @@ module.exports = {
     extend: {}
   },
   darkMode: 'class',
-  plugins: [nextui()]
+  plugins: [
+    nextui({
+      prefix: 'nextui',
+      defaultTheme: 'dark',
+      themes: {
+        'virgil-theme': {
+          extend: 'dark',
+          colors: {
+            background: '#282936',
+            foreground: '#dddddf',
+            primary: {
+              DEFAULT: '#a58ef5',
+              foreground: '#dddddf'
+            },
+            secondary: {
+              DEFAULT: '#282936',
+              foreground: '#dddddf'
+            },
+            focus: '#1d1e27'
+          },
+          layout: {
+            disabledOpacity: '0.3',
+            radius: {
+              small: '4px',
+              medium: '6px',
+              large: '8px'
+            },
+            borderWidth: {
+              small: '1px',
+              medium: '2px',
+              large: '3px'
+            }
+          }
+        }
+      }
+    })
+  ]
 }
