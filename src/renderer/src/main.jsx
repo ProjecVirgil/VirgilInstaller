603a71161f70d3ca122c52acdf393796188434d2
Retr0 <dragonfaiver@gmail.com>
2023-12-06 22:59:31 +0100

    Add router

--- src/renderer/src/main.jsx
+++ src/renderer/src/main.jsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client'
 import './assets/index.css'
 import { NextUIProvider } from '@nextui-org/react'
 import App from './App'
+import { createBrowserRouter } from 'react-router-dom'
+//import { closeWindow } from 'electron';
+
+const router = createBrowserRouter([
+  // Add your routes here, e.g.,
+  {
+    path: '/',
+    element: <App />,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
