f5e1dfd53a967a0c2da240472c67cb3b0b88b83a
Retr0100 <dragonfaiver@gmail.com0>
2023-12-06 11:47:38 +0100

    Finished the stepper component now working 100%

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -7,24 +7,33 @@ import Box from '@mui/material/Box'
 import Stepper from '@mui/material/Stepper'
 import Step from '@mui/material/Step'
 import StepLabel from '@mui/material/StepLabel'
-import Typography from '@mui/material/Typography'
+import PropTypes from 'prop-types'
+import StepConnector, { stepConnectorClasses } from '@mui/material/StepConnector'
+import Check from '@mui/icons-material/Check'
+import { styled } from '@mui/material/styles'
 
 const steps = ['Step 1', 'Step 2', 'Step 3', 'Step 4']
 
 const stepStyle = {
-  '& .Mui-activate': {
-    '&.MuiStepIcon-root': {
-      color: 'rgba(165, 142, 245, 1)',
-      fontSize: '20px'
+  '& .Mui-active': {
+    '&.MuiStepLabel-label': {
+      color: 'rgba(221, 221, 223, 1)',
+      fontSize: '15px'
     }
   },
   '& .Mui-completed': {
-    '&.MuiStepIcon-root': {
-      color: 'rgba(165, 142, 245, 1)',
-      fontSize: '20px'
+    '&.MuiStepLabel-label': {
+      color: 'rgba(221, 221, 223, 1)',
+      fontSize: '15px'
+    }
+  },
+  '& .Mui-disabled': {
+    '&.MuiStepLabel-label': {
+      color: 'rgba(221, 221, 223, 1)',
+      fontSize: '15px'
     }
   }
-}
+} //MuiStepLabel-label Mui-disabled css-1hv8oq8-MuiStepLabel-label
 function App() {
   const [activeStep, setActiveStep] = React.useState(0)
   const [skipped, setSkipped] = React.useState(new Set())
@@ -52,23 +61,99 @@ function App() {
     setActiveStep((prevActiveStep) => prevActiveStep - 1)
   }
 
+  const QontoConnector = styled(StepConnector)(({ theme }) => ({
+    [`&.${stepConnectorClasses.alternativeLabel}`]: {
+      top: 10,
+      left: 'calc(-50% + 16px)',
+      right: 'calc(50% + 16px)'
+    },
+    [`&.${stepConnectorClasses.active}`]: {
+      [`& .${stepConnectorClasses.line}`]: {
+        borderColor: '#A58EF5'
+      }
+    },
+    [`&.${stepConnectorClasses.completed}`]: {
+      [`& .${stepConnectorClasses.line}`]: {
+        borderColor: '#A58EF5'
+      }
+    },
+    [`& .${stepConnectorClasses.line}`]: {
+      borderColor: theme.palette.mode === 'dark' ? theme.palette.grey[800] : '#eaeaf0',
+      borderTopWidth: 3,
+      borderRadius: 1
+    }
+  }))
+
+  const QontoStepIconRoot = styled('div')(({ theme, ownerState }) => ({
+    color: theme.palette.mode === 'dark' ? theme.palette.grey[700] : '#eaeaf0',
+    display: 'flex',
+    height: 22,
+    alignItems: 'center',
+    ...(ownerState.active && {
+      color: '#A58EF5'
+    }),
+    '& .QontoStepIcon-completedIcon': {
+      color: '#A58EF5',
+      zIndex: 1,
+      fontSize: 22
+    },
+    '& .QontoStepIcon-circle': {
+      width: 8,
+      height: 8,
+      borderRadius: '50%',
+      backgroundColor: 'currentColor'
+    }
+  }))
+
+  function QontoStepIcon(props) {
+    const { active, completed, className } = props
+
+    return (
+      <QontoStepIconRoot ownerState={{ active }} className={className}>
+        {completed ? (
+          <Check className="QontoStepIcon-completedIcon" />
+        ) : (
+          <div className="QontoStepIcon-circle" />
+        )}
+      </QontoStepIconRoot>
+    )
+  }
+
+  QontoStepIcon.propTypes = {
+    /**
+     * Whether this step is active.
+     * @default false
+     */
+    active: PropTypes.bool,
+    className: PropTypes.string,
+    /**
+     * Mark the step as completed. Is passed to child components.
+     * @default false
+     */
+    completed: PropTypes.bool
+  }
+
   return (
     <div className="h-screen w-[100%]">
       <NavbarMain></NavbarMain>
       <Box sx={{ width: '80%', marginX: '50px', marginY: '6px' }}>
-        <Stepper activeStep={activeStep} sx={stepStyle}>
-          {steps.map((label, index) => (
-            <Step key={label} >
-              <StepLabel>{label}</StepLabel>
+        <Stepper activeStep={activeStep} sx={stepStyle} connector={<QontoConnector />}>
+          {steps.map((label) => (
+            <Step key={label}>
+              <StepLabel StepIconComponent={QontoStepIcon}>{label}</StepLabel>
             </Step>
           ))}
         </Stepper>
       </Box>
       <Main></Main>
-      <DivBStep handleBack={handleBack} handleNext={handleNext}></DivBStep>
+      <DivBStep
+        nextLabel= {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+        prevLabel="Prev"
+        handleBack={handleBack}
+        handleNext={handleNext}
+      ></DivBStep>
     </div>
   )
 }
 
 export default App
-//completed={completed[index]}
--- src/renderer/src/components/DivBStep.jsx
+++ src/renderer/src/components/DivBStep.jsx
@@ -1,11 +1,11 @@
 import BStep from './BStep'
 
 function DivBStep(props) {
-  const { handleNext, handleBack } = props
+  const { handleNext, handleBack,nextLabel,prevLabel } = props
   return (
     <div className="w-[100%] h-[36px] flex justify-end mt-[10px] ">
-      <BStep handle={handleBack} label="Prev"></BStep>
-      <BStep handle={handleNext} label="Next" Isfirst={true}></BStep>
+      <BStep handle={handleBack} label={prevLabel}></BStep>
+      <BStep handle={handleNext} label={nextLabel} Isfirst={true}></BStep>
     </div>
   )
 }
