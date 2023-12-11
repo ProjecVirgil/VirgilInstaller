33790836b1384a9494c56e231a10e297ea1c827a
Retr0 <dragonfaiver@gmail.com>
2023-12-11 22:11:44 +0100

    refactor: refactor Main component
    Refactor the Main component to use functional components and hooks for better readability and performance. Also, add a scroll functionality to automatically scroll to the bottom when a reminder is received.
    
    Changes:
    - Convert Main component to a functional component
    - Add useRef and useEffect hooks to handle scrolling functionality
    - Replace hardcoded headings with corresponding components
    - Update the return statement with the ScrollShadow component and content function

--- src/renderer/src/components/Main.jsx
+++ src/renderer/src/components/Main.jsx
@@ -1,20 +1,51 @@
+import Welcome from './WelcomeScene'
+import { ScrollShadow } from '@nextui-org/react'
+import License from './LicenseScene'
+import ConfigScene from './ConfigScene'
+import React from 'react'
+
 function Main(props) {
   const step = props.step
+  const scrollRef = React.useRef(null);
+
+  const scrollToBottom = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+    }
+  };
+
+  React.useEffect(() => {
+    const reminderListener = () => {
+      scrollToBottom();
+    };
+    window.api.receive('reminder', reminderListener);
+  }, []);
+
   const content = () => {
     if (step == 0) {
-      return <h1>Benvenuto</h1>
+      return <Welcome></Welcome>
     } else if (step == 1) {
-      return <h1>Licenza</h1>
+      return <License></License>
     } else if (step == 2) {
-      return <h1>Installazione</h1>
+      return <ConfigScene></ConfigScene>
     } else if (step == 3) {
-      return <h1>Config locale e creazione chiave</h1>
-    } else if (step == 4) {
-      return <h1>Config locale e creazione chiave</h1>
+      return <h1>Installazione</h1>
     }
   }
 
-  return <div className="main">{content()}</div>
+  return (
+    <div className="main">
+      <ScrollShadow
+        ref={scrollRef}
+        offset={100}
+        size={20}
+        hideScrollBar
+        className="w-[100%] h-[99%] rounded-[24px] p-5 change_font scroll-smooth"
+      >
+        {content()}
+      </ScrollShadow>
+    </div>
+  )
 }
 
 export default Main
