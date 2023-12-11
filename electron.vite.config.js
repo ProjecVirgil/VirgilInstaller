7371c99410dfe1702ff10c4c6ccc5e19a1f153eb
Retr0 <dragonfaiver@gmail.com>
2023-12-11 22:05:13 +0100

    feat(Plugin and Config): Added a plugin for CSS and the acces a some online resource

--- electron.vite.config.js
+++ electron.vite.config.js
@@ -1,6 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
+import cssnano from 'cssnano'
 
 export default defineConfig({
   main: {
@@ -9,12 +10,22 @@ export default defineConfig({
   preload: {
     plugins: [externalizeDepsPlugin()]
   },
+  server: {
+    headers: {
+      'Content-Security-Policy': "default-src 'self'; img-src 'self' https://img.shields.io;"
+    }
+  },
   renderer: {
     resolve: {
       alias: {
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [react()]
+    plugins: [
+      react(),
+      cssnano({
+        preset: 'default'
+      })
+    ]
   }
 })
