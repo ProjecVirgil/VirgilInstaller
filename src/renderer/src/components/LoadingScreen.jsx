227ba1ce23587f40150bb5be318b869288b3bdc2
Retr0100 <dragonfaiver@gmail.com0>
2023-12-12 11:29:53 +0100

    feat: Add image download functionality to LoadingScreen component

--- src/renderer/src/components/LoadingScreen.jsx
+++ src/renderer/src/components/LoadingScreen.jsx
@@ -1,11 +1,27 @@
 import { Spinner } from '@nextui-org/react'
+import { useEffect } from 'react'
 
-const LoadingScreen = () => (
-  <div className="loading-container">
-    <div className="loader">
-      <Spinner label="Loading..." color="primary" size="lg" />
+const LoadingScreen = () => {
+
+  useEffect(() => {
+    const handleDownloadResponse = (event, message) => {
+      console.log(message)
+    }
+
+    window.api.receive('imageDownloaded', handleDownloadResponse)
+    window.api.receive('imageDownloadError', handleDownloadResponse)
+
+    window.api.send('downloadImage')
+
+  }, [])
+
+  return (
+    <div className="loading-container">
+      <div className="loader">
+        <Spinner label="Loading..." color="primary" size="lg" />
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default LoadingScreen
