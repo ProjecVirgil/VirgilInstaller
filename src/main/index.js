39fac3f00bb74b918cc52debb47f81bd3cba3e26
Retr0 <dragonfaiver@gmail.com>
2023-12-12 22:13:46 +0100

    feat: Add image downloading functionality
    This commit adds the ability to download images using the app. It uses the fs module to save the images locally, axios for making HTTP requests, and cheerio for parsing HTML content. The image URLs are hardcoded for now, but will be dynamically fetched in future updates.
    
    The commit also includes a function check_if_update(), which compares the current version of the app with the latest version available on GitHub. If an update is available, it will return true. This functionality will be used to notify users of new updates.
    
    This commit should trigger a release bumping a MINOR version.

--- src/main/index.js
+++ src/main/index.js
@@ -2,6 +2,9 @@ import { app, BrowserWindow, ipcMain, dialog } from 'electron'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import path from 'path'
 import icon from '../../resources/icons/icon.png'
+import fs from 'fs'
+import axios from 'axios'
+import cheerio from 'cheerio'
 
 let mainWindow
 function createWindow() {
@@ -62,7 +65,6 @@ app.whenReady().then(() => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
-
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
@@ -95,8 +97,6 @@ ipcMain.on('open-file-dialog', async (event) => {
   }
 })
 
-const fs = require('fs')
-const axios = require('axios')
 ipcMain.on('downloadImage', (event) => {
   const imageUrls = [
     'https://img.shields.io/badge/2%2C1k-2%2C1k?style=for-the-badge&logo=visualstudiocode&label=Lines%20of%20code&labelColor=282a3&color=%23164773',
@@ -123,4 +123,47 @@ ipcMain.on('downloadImage', (event) => {
         )
       })
   }
+})
+
+function check_if_update(actual, last_version) {
+  if (actual === last_version) {
+    return false
+  }
+
+  const parse_version_actual = actual
+    .replace('v', '')
+    .split('.')
+    .map((str) => parseInt(str, 10))
+  const parse_last_version = last_version
+    .replace('v', '')
+    .split('.')
+    .map((str) => parseInt(str, 10))
+
+  if (parse_version_actual[0] < parse_last_version[0]) {
+    return true
+  } else {
+    if (parse_version_actual[1] < parse_last_version[1]) {
+      return true
+    } else {
+      if (parse_version_actual[2] < parse_last_version[2]) {
+        return true
+      } else {
+        return false
+      }
+    }
+  }
+}
+ipcMain.on('search_update', (event) => {
+  axios.get('https://github.com/ProjecVirgil/VirgilAI/tags').then((response) => {
+    const $ = cheerio.load(response.data)
+    const content = $('body').html()
+    const regex = /v\d+\.\d+\.\d+/ //add g for more results
+    const results = content.match(regex)
+    const last_version = results[0] //last version
+    const packageJsonPath = path.join('package.json')
+    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'))
+    const actual_version = packageJson.version
+    const to_update = check_if_update(actual_version, last_version)
+    event.sender.send('result_check', to_update)
+  })
 })
