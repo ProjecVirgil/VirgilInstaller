3b495e7f73b3a293ba26e9e973d5357ace5bf2aa
Retr0 <dragonfaiver@gmail.com>
2023-12-12 22:13:56 +0100

    feat: Remove unnecessary import statement in App.jsx

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import NavbarMain from './components/NavbarMain'
 import Main from './components/Main'
 import DivBStep from './components/DivBStep'
