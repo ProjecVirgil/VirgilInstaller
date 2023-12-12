d0a2fb7fb2a5ed8c3af224c1e945fc624a1f7314
Retr0 <dragonfaiver@gmail.com>
2023-12-12 22:27:34 +0100

    feat: Update steps in App component
    Updated the steps array in the App component from ['Step 1', 'Step 2', 'Step 3', 'Step 4', 'Step 5'] to ['Welcome', 'License', 'Configuration', 'Installation']. This change enhances the user experience by providing more meaningful step names.

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -23,7 +23,7 @@ window.api.receive('checked', () => {
   isSelected = !isSelected
 })
 
-const steps = ['Step 1', 'Step 2', 'Step 3', 'Step 4', 'Step 5']
+const steps = ['Welcome', 'License', 'Configuration', 'Installation']
 
 const stepStyle = {
   '& .Mui-active': {
