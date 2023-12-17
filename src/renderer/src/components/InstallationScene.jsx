444cba3a41c1cdfe43b5c22816d78b7227d0deba
Retr0 <dragonfaiver@gmail.com>
2023-12-17 21:34:50 +0100

    feat: Add task list and task status in InstallationScene display their status.  functions to add tasks to the list and change their status. - Implemented useEffect hook to handle command output and update task status accordingly. - Updated JSX to render the task list and display the status icons. - Added useRef hook to scroll to the end of the output div.

--- src/renderer/src/components/InstallationScene.jsx
+++ src/renderer/src/components/InstallationScene.jsx
@@ -1,28 +1,79 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
+
+// Supponendo che queste importazioni siano corrette
+import PendingIcon from './PendingIcon'
+import ErrorIcon from '@mui/icons-material/Error'
+import DoneIcon from './DoneIcon'
+
+const command_list = ['echo ciao', 'echo mammt']
 
 function InstallationScene() {
-  const [console_output, setConsole_output] = useState('')
+  const outputEndRef = useRef(null)
+  const [list_task, setListTask] = useState([])
+  const [currentTaskIndex, setCurrentTaskIndex] = useState(0)
+
+  function addTask() {
+    setListTask((prevLista) => [...prevLista, { status: 0 }])
+  }
+
+  function changeStatus(new_status, index) {
+    setListTask((prev_list) => {
+      const updated_list = [...prev_list]
+      updated_list[index] = { ...updated_list[index], status: new_status }
+      return updated_list
+    })
+  }
 
   useEffect(() => {
-    const handleOutput = (message) => {
-      setConsole_output(message)
+    // Funzione per gestire l'output del comando
+    const handleOutput = () => {
+      console.log('OUT')
+      changeStatus(2, currentTaskIndex) // Imposta lo stato del task corrente su completato
+      setCurrentTaskIndex((currentIndex) => currentIndex + 1) // Passa al task successivo
     }
 
-    window.api.send('runcommand')
+    // Registra il listener per l'evento di output
     window.api.receive('outputcommand', handleOutput)
 
-  }, [console_output])
+    // Esegue il task corrente
+    function runCurrentTask() {
+      if (currentTaskIndex < command_list.length) {
+        addTask()
+        changeStatus(1, currentTaskIndex)
+        window.api.send('runcommand', command_list[currentTaskIndex])
+      }
+    }
+
+    runCurrentTask()
+  }, [currentTaskIndex])
 
   return (
     <div>
       <h1 className="text-center text-[24px] m-3 mt-0">Installation in progress</h1>
       <div className="m-6 bg-black opacity-70 p-4">
-        <p
-          className="text-[13px] text-[#dddddf]"
-          dangerouslySetInnerHTML={{ __html: console_output }}
-        ></p>
+        <ul className="text-[13px] text-[#dddddf]">
+          {list_task.map((item, index) => (
+            <li key={index}>
+              <div>
+                {item.status === 2 ? (
+                  <div className="flex">
+                    <p className="mr-2 mb-1">Operazione numero: {index + 1} fatta</p>
+                    <DoneIcon size="18px" />
+                  </div>
+                ) : (
+                  <div className="flex">
+                    <p className="mr-2 mb-1">Operazione numero: {index + 1} in attesa</p>
+                    <PendingIcon size="17px" />
+                  </div>
+                )}
+              </div>
+            </li>
+          ))}
+        </ul>
+        <div ref={outputEndRef} />
       </div>
     </div>
   )
 }
+
 export default InstallationScene
