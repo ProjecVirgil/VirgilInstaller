7c9fc1aae68892185a3bb7adb20cf27396831bcc
Retr0 <dragonfaiver@gmail.com>
2023-12-22 18:52:59 +0100

    feat: Add JsonManager utility functions
    This commit adds the utility functions getJSON and setJSON to the JsonManager module. These functions allow the user to get and set JSON data using the Electron API. This feature will trigger a release and bump the MINOR version.

---
+++ src/renderer/src/utils/JsonManager.jsx
@@ -0,0 +1,21 @@
+export function getJSON(file_path) {
+  return new Promise((resolve, reject) => {
+    const handleJSON = (data) => {
+      resolve(data)
+    }
+
+    window.api.receiveOnce('gettedJSON', handleJSON)
+    window.api.send('getJSON', file_path)
+  })
+}
+
+export function setJSON(file_path, new_data) {
+  return new Promise((resolve, reject) => {
+    const handleJSON = (data) => {
+      resolve(data)
+    }
+
+    window.api.receiveOnce('settedJSON', handleJSON)
+    window.api.send('setJSON', [file_path, new_data])
+  })
+}
