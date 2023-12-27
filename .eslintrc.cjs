20dbe4ca1736a41a6d050f1e828fab9d29fe5f5a
Retr0 <dragonfaiver@gmail.com>
2023-12-27 23:15:27 +0100

    style: Add prettier rule for endOfLine

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,5 +5,8 @@ module.exports = {
     'plugin:react/jsx-runtime',
     '@electron-toolkit',
     '@electron-toolkit/eslint-config-prettier'
-  ]
+  ],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }]
+  }
 }
