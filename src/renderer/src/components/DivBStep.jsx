45933f050809be4306d0845e24899e1d4064208f
Retr0 <dragonfaiver@gmail.com>
2023-12-27 11:35:11 +0100

    feat: Add disable prop to DivBStep component
    This commit adds a new prop "isDisable" to the DivBStep component, allowing the component to be disabled when necessary. This feature enhances the usability and flexibility of the component.

--- src/renderer/src/components/DivBStep.jsx
+++ src/renderer/src/components/DivBStep.jsx
@@ -1,13 +1,15 @@
 import BStep from './BStep'
 
 function DivBStep(props) {
-  const { handleNext, handleBack, nextLabel, prevLabel, isVisible } = props
+  const { handleNext, handleBack, nextLabel, prevLabel, isVisible, isDisable } = props
 
   return (
     <div className="w-[100%] h-[36px] flex justify-end mt-[10px]">
-      <BStep handle={handleBack} label={prevLabel} />
       {isVisible ? (
-        <BStep handle={handleNext} label={nextLabel} Isfirst={true} />
+        <>
+          <BStep handle={handleBack} disable={isDisable} label={prevLabel} />
+          <BStep handle={handleNext} disabled={isDisable} label={nextLabel} Isfirst={true} />
+        </>
       ) : (
         <BStep handle={() => window.api.send('close')} label="Close" Isfirst={true} />
       )}
