84b9c6acf9dc663d34eac99d6964dbc47f335c91
Retr0 <dragonfaiver@gmail.com>
2024-01-03 17:13:31 +0100

    feat: Add functionality to set window title
    This commit adds the functionality to set the window title to 'Virgil Installer v2.0.0' when the main window is ready to show.

--- src/main/index.js
+++ src/main/index.js
@@ -38,6 +38,7 @@ function createWindow() {
 
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
+    mainWindow.setTitle('Virgil Installer v2.0.0')
   })
 
   // HMR for renderer base on electron-vite cli.
