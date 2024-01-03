6b1c9a89e2b9b18f3af83bf7e007b18bf4fd7669
Retr0 <dragonfaiver@gmail.com>
2024-01-03 14:42:57 +0100

    feat: Add config loading in FinishScene component
    This commit adds the functionality to load the config.json file in the FinishScene component. The config data is fetched using the getJSON utility function and stored in the state using the useState hook. The loaded config key is displayed in the component's JSX.

--- src/renderer/src/components/FinishScene.jsx
+++ src/renderer/src/components/FinishScene.jsx
@@ -1,9 +1,21 @@
-import { ScrollShadow } from '@nextui-org/react'
+import { ScrollShadow, Snippet } from '@nextui-org/react'
 import gif from '../assets/img/giphy.gif'
-import { useContext } from 'react'
-import { MainContext } from '../context/MainContext'
+import { useEffect, useState } from 'react'
+import { getJSON } from '../utils/JsonManager'
+
 function FinishScene() {
-  const { config } = useContext(MainContext)
+  const [config, setConfig] = useState(null)
+
+  useEffect(() => {
+    getJSON('config.json')
+      .then((data) => {
+        console.log('Data loaded:', data)
+        setConfig(data)
+      })
+      .catch((error) => {
+        console.error('Error during the load of config', error)
+      })
+  }, [])
 
   return (
     <div>
@@ -11,33 +23,43 @@ function FinishScene() {
         <ScrollShadow
           offset={100}
           size={20}
-          className="w-[100%] h-[99%] rounded-[24px] p-5 change_font scroll-smooth "
+          className="w-[100%] h-[99%] rounded-[24px] p-5 change_font scroll-smooth"
         >
-          <h1 className="text-center text-[20px]">INSTALLATION COMPLETE</h1>
-          <p className="text-[12px] mb-3">
-            Ok now you follow the guide on this{' '}
-            <a
-              target="_blank"
-              className="text-link"
-              href="https://github.com/ProjecVirgil/VirgilAI?tab=readme-ov-file#guide-to-online-settings"
-              rel="noreferrer"
-            >
-              link
-            </a>{' '}
-            and setup all the settings with your{' '}
-            <a
-              className="text-link"
-              target="_blank"
-              href="https://github.com/ProjecVirgil/VirgilApp"
-              rel="noreferrer"
-            >
-              mobile app
-            </a>
-            <br />
-            YOUR VIRGIL KEY: <strong>{config.key}</strong>
-          </p>
-          <div className="w-[100%] justify-center flex">
-            <img src={gif} width={300} className="mt-1" alt="" />
+          <div className="text-center">
+            <h1 className="text-[24px] font-bold mb-4">INSTALLATION COMPLETE</h1>
+            <p className="text-[16px] leading-relaxed mb-4">
+              Ok, now you can follow the guide on this{' '}
+              <a
+                target="_blank"
+                className="text-link underline text-[#a58ef5] hover:text-[#b59ff6]"
+                href="https://github.com/ProjecVirgil/VirgilAI?tab=readme-ov-file#guide-to-online-settings"
+                rel="noreferrer"
+              >
+                link
+              </a>{' '}
+              and set up all the settings with your{' '}
+              <a
+                className="text-link underline text-[#a58ef5] hover:text-[#b59ff6]"
+                target="_blank"
+                href="https://github.com/ProjecVirgil/VirgilApp"
+                rel="noreferrer"
+              >
+                mobile app
+              </a>.
+              <br />
+              <br />
+              <span className="text-[18px] font-bold mb-4">YOUR VIRGIL KEY:</span>{' '}
+              {config ? (
+                <Snippet color="primary" size="sm" className="text-[12px]">
+                  {config.key}
+                </Snippet>
+              ) : (
+                <span>Loading key...</span>
+              )}
+            </p>
+            <div className="flex justify-center">
+              <img src={gif} width={300} className="mt-1" alt="Installation process animation" />
+            </div>
           </div>
         </ScrollShadow>
       </div>
